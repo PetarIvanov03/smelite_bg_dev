0a3978e4469bd4ffb3a7f79a7567a65cd44e4ec3
PetarIvanov03 <petarivanov03it@gmail.com>
2025-07-15 17:27:51 +0300

    33

--- smelite_app/smelite_app/Controllers/CraftsController.cs
+++ smelite_app/smelite_app/Controllers/CraftsController.cs
@@ -16,10 +16,6 @@ namespace smelite_app.Controllers
 
         public async Task<IActionResult> Index(int? craftTypeId, int? locationId, string? search)
         {
-            var sender = new EmailSender();
-            bool success = await sender.SendEmailAsync("i.petarivanov03@gmail.com", "Test", "Test message");
-            Console.WriteLine(success ? "Sent!" : "Failed!");
-
             var crafts = await _craftService.GetFilteredCraftsAsync(craftTypeId, locationId, search);
             var items = crafts.Select(c => new CraftListItemViewModel
             {
--- smelite_app/smelite_app/obj/Debug/net8.0/smelite_app.AssemblyInfo.cs
+++ smelite_app/smelite_app/obj/Debug/net8.0/smelite_app.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smelite_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ec068464e74f9402512272a6d835f2a1a8943d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d632dbbc14af295a25c769d7907e14569d139bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("smelite_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smelite_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
