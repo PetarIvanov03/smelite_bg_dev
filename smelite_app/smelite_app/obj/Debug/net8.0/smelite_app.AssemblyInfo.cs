c2911da51b0ba50aa9d75c39030b322e64639915
PetarIvanov03 <petarivanov03it@gmail.com>
2025-07-16 21:58:26 +0300

    5555

--- smelite_app/smelite_app/obj/Debug/net8.0/smelite_app.AssemblyInfo.cs
+++ smelite_app/smelite_app/obj/Debug/net8.0/smelite_app.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("smelite_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdf7ae0e0c88e41fa268ec6fc5262c7043c913d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d843234f908ec81b9f6052c4dd857175c0a2bb45")]
 [assembly: System.Reflection.AssemblyProductAttribute("smelite_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("smelite_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
