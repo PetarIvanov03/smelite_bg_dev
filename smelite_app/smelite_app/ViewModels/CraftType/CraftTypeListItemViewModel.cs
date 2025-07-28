d38c776d5dc0dbd2173e1e3d26e1184311249ab5
Petar Ivanov <160013791+PetarIvanov03@users.noreply.github.com>
2025-07-28 17:37:05 +0300

    Use view models for CraftType CRUD

---
+++ smelite_app/smelite_app/ViewModels/CraftType/CraftTypeListItemViewModel.cs
@@ -0,0 +1,8 @@
+namespace smelite_app.ViewModels.CraftType
+{
+    public class CraftTypeListItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
